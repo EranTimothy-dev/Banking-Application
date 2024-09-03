9c2fb4655798c0cb4e0e8397f60e24df70173129
EranTimothy-dev <eranperera.dev@gmail.com>
2024-09-03 15:50:44 +0530

    added section to view the transactions

--- src/main/java/commercial/bankingapplication/Views/ViewFactory.java
+++ src/main/java/commercial/bankingapplication/Views/ViewFactory.java
@@ -8,12 +8,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+// class to create methods handling the view of the application
 public class ViewFactory {
     //Client Views
     private AnchorPane dashboardView;
+    private AnchorPane transactionsView;
 
     public ViewFactory(){}
 
+    // load the dashboard fxml
     public AnchorPane getDashboardView(){
         if(dashboardView == null){
             try {
@@ -25,11 +28,25 @@ public class ViewFactory {
         return dashboardView;
     }
 
+    // load the transaction fxml
+    public AnchorPane getTransactionsView(){
+        if(transactionsView == null){
+            try{
+                transactionsView = new FXMLLoader(getClass().getResource("/FXML/Client/Transactions.fxml")).load();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return transactionsView;
+    }
+
+    // get the login window
     public void showLoginWindow(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
         createStage(loader);
     }
 
+    // get the client window
     public void showClientWindow(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/Client.fxml"));
         ClientController clientController = new ClientController();
@@ -37,6 +54,7 @@ public class ViewFactory {
         createStage(loader);
     }
 
+    // create reusable method to load the fxml file and create the stage from it
     private void createStage(FXMLLoader loader) {
         Scene scene = null;
         try{
@@ -49,6 +67,8 @@ public class ViewFactory {
         stage.setTitle("Banking Application");
         stage.show();
     }
+
+    // create method to close the stage
     public void closeStage(Stage stage){
         stage.close();
     }
