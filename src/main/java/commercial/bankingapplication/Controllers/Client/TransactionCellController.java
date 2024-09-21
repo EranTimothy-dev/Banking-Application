c33bede674b1ea2ed24aae6345199db47806ce1d
EranTimothy-dev <eranperera.dev@gmail.com>
2024-09-22 00:10:39 +0530

    transaction description lisings section

--- src/main/java/commercial/bankingapplication/Controllers/Client/TransactionCellController.java
+++ src/main/java/commercial/bankingapplication/Controllers/Client/TransactionCellController.java
@@ -1,2 +1,22 @@
-package commercial.bankingapplication.Controllers.Client;public class TransactionCellController {
+package commercial.bankingapplication.Controllers.Client;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransactionCellController implements Initializable {
+
+
+    public FontAwesomeIconView in_icon;
+    public FontAwesomeIconView out_icon;
+    public Label transaction_lbl;
+    public Label sender_lbl;
+    public Label receiver_lbl;
+    public Label amount_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
 }
